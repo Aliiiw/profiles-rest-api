0c0e872b51e36485a47ee0a26625fb5d15ed0489
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-04 15:09:03 +0430

    create hello url

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ Api testing class"""
+    
+    def get(self, request, format = None):
+        api_view = [
+            'Uses Api functions',
+            'like Djagno views',
+            'control on logic',
+            'handeled URLs ',
+        ]
+        return Response({'message' : 'Hello!','an_api': api_view})
+    
+    
